6f67778b38603bc66eec0a7ca3b3cac4fd5df891
jhyg2 <110159420+jhyg2@users.noreply.github.com>
2022-08-26 17:37:18 +0900

    commit message test

---
+++ frontend/src/components/C1Command.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            C1
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="c1"
+            >
+                C1
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'C1Command',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            c1() {
+                this.$emit('c1', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
